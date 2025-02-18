74bc5340c4a151631cbf37cebcee64b8aaeef769
rduo1009 <robertduong09alt@gmail.com>
2025-02-18 20:07:25 +1100

    🚨 tests: adding invalid config integration test

--- tests/integration/server_integration/server_integration_test.py
+++ tests/integration/server_integration/server_integration_test.py
@@ -15,11 +15,7 @@ import requests
 from src.__main__ import cli
 
 
-def _remove_first_n_chars(text: str, n: int) -> str:
-    return "\n".join(line[n:] for line in text.splitlines())
-
-
-def _run_cli(port):
+def run_cli(port):
     with contextlib.suppress(KeyboardInterrupt):
         cli(["-v", "-v", "-v", "-p", str(port)])
 
@@ -33,10 +29,10 @@ def test_cli_normal(caplog, snapshot, monkeypatch):
     vocab_file = Path(__file__).parent / "testdata" / "test-regular-list.txt"
     vocab_list = vocab_file.read_text(encoding="utf-8")
 
-    session_config_file = Path(__file__).parent / "testdata" / "test-config.json"
+    session_config_file = Path(__file__).parent / "testdata" / "test-regular-config.json"
     session_config = json.loads(session_config_file.read_text(encoding="utf-8"))
 
-    cli_process = threading.Thread(target=_run_cli, args=(5500,), daemon=True)
+    cli_process = threading.Thread(target=run_cli, args=(5500,), daemon=True)
     cli_process.start()
 
     try:
@@ -59,18 +55,18 @@ def test_cli_normal(caplog, snapshot, monkeypatch):
 
 
 @pytest.mark.integration
-def test_cli_list_badpos(caplog, snapshot, monkeypatch):
+def test_cli_error_list(caplog, snapshot, monkeypatch):
     monkeypatch.setattr("src.server.app.vocab_list", None)
 
     server_url = "http://127.0.0.1:5501"
 
-    vocab_file = Path(__file__).parent / "testdata" / "test-badpos-list.txt"
+    vocab_file = Path(__file__).parent / "testdata" / "test-error-list.txt"
     vocab_list = vocab_file.read_text(encoding="utf-8")
 
-    session_config_file = Path(__file__).parent / "testdata" / "test-config.json"
+    session_config_file = Path(__file__).parent / "testdata" / "test-regular-config.json"
     session_config = json.loads(session_config_file.read_text(encoding="utf-8"))
 
-    cli_process = threading.Thread(target=_run_cli, args=(5501,), daemon=True)
+    cli_process = threading.Thread(target=run_cli, args=(5501,), daemon=True)
     cli_process.start()
 
     try:
@@ -78,6 +74,52 @@ def test_cli_list_badpos(caplog, snapshot, monkeypatch):
 
         vocab_response = requests.post(f"{server_url}/send-vocab", data=vocab_list, timeout=5)
         assert vocab_response.status_code == 400
+        assert vocab_response.text == snapshot
+
+        try:
+            session_response = requests.post(f"{server_url}/session", json=session_config, timeout=5)
+            session_response.raise_for_status()
+
+            pytest.fail("Expected an error but request succeeded.")
+
+        except requests.exceptions.HTTPError:
+            if session_response.status_code == 400:
+                assert session_response.status_code == 400
+                assert session_response.text == snapshot
+            else:
+                raise
+
+        except requests.exceptions.RequestException:
+            raise
+
+    finally:
+        sleep(5)
+
+        cli_process.join(timeout=10)
+
+    assert caplog.text == snapshot
+
+
+@pytest.mark.integration
+def test_cli_error_config(caplog, snapshot, monkeypatch):
+    monkeypatch.setattr("src.server.app.vocab_list", None)
+
+    server_url = "http://127.0.0.1:5502"
+
+    vocab_file = Path(__file__).parent / "testdata" / "test-regular-list.txt"
+    vocab_list = vocab_file.read_text(encoding="utf-8")
+
+    session_config_file = Path(__file__).parent / "testdata" / "test-error-config.json"
+    session_config = json.loads(session_config_file.read_text(encoding="utf-8"))
+
+    cli_process = threading.Thread(target=run_cli, args=(5502,), daemon=True)
+    cli_process.start()
+
+    try:
+        sleep(5)
+
+        vocab_response = requests.post(f"{server_url}/send-vocab", data=vocab_list, timeout=5)
+        assert vocab_response.status_code == 200
         assert vocab_response.text == snapshot
 
         try:
