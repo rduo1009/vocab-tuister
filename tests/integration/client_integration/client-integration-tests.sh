37ce60a37dbaaf29ef8f431e33b6498f98d1b302
rduo1009 <robertduong09alt@gmail.com>
2025-03-05 19:09:05 +1100

    🚨 tests: attempting to fix issue

--- tests/integration/client_integration/client-integration-tests.sh
+++ tests/integration/client_integration/client-integration-tests.sh
@@ -6,7 +6,7 @@ export PATH="$(pwd):$PATH"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 go build \
-    -ldflags "-X github.com/rduo1009/vocab-tuister/src/client/internal.Version=$(dunamai from any)" \
+    -ldflags "-X github.com/rduo1009/vocab-tuister/src/client/internal.Version=$(poetry run dunamai from any)" \
     -cover \
     -o ./vocab-tuister \
     ./src
