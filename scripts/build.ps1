fa9cd1bd9a44efc51ca8fcc265fe3a42794682f1
rduo1009 <robertduong09alt@gmail.com>
2025-05-11 22:37:15 +1000

    🐛 fix: testing again 3

---
+++ scripts/build.ps1
@@ -0,0 +1,51 @@
+#Requires -Version 5.1
+# Stop on first error
+$ErrorActionPreference = "Stop"
+
+if ($env:debug -eq "True") {
+    Write-Host "====== DEBUG MODE ======"
+}
+
+# Only install necessary deps to speed up build
+# poetry install --only main --sync # slower
+poetry env remove --all
+poetry install --only main
+
+# Build python server
+poetry run dunamai from any | Set-Content -Path "__version__.txt"
+if ($env:debug -eq "True") {
+    poetry run pyinstaller vocab-tuister-server.spec --clean -- --debug
+}
+else {
+    poetry run pyinstaller vocab-tuister-server.spec --clean
+}
+
+# Determine client binary name
+$clientbin_name = ""
+if ($IsWindows) {
+    $clientbin_name = "windows"
+}
+else {
+    Write-Host "Unknown OS"
+    exit 1
+}
+
+# Build go client
+$version = (dunamai from any)
+go mod tidy
+go generate -x src/generate.go
+go build `
+    -ldflags "-X github.com/rduo1009/vocab-tuister/src/client/internal.Version=$version" `
+    -o ".\dist\vocab-tuister-$clientbin_name.exe" `
+    .\src\main.go
+
+# Write-Host "Do you want to reinstall all deps? (Y/n)"
+# $response = Read-Host
+# Write-Host ""
+# if ([string]::IsNullOrEmpty($response)) { $response = "Y" }
+# if ($response -eq "y" -or $response -eq "Y") {
+#     poetry install --sync
+# }
+# else {
+#     poetry install --only main --sync
+# }
