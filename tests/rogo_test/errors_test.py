9c67bee3ff7d0385aa533197d20bad5a8d8bf5b0
rduo1009 <robertduong09alt@gmail.com>
2025-06-07 15:04:12 +1000

    🚨 tests: adding test for question asker error

--- tests/rogo_test/errors_test.py
+++ tests/rogo_test/errors_test.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from typing import TYPE_CHECKING
 
 import pytest
+from src.core.lego.misc import VocabList
 from src.core.lego.reader import read_vocab_file
 from src.core.rogo.asker import ask_question_without_sr
 from src.core.rogo.exceptions import InvalidSettingsError
@@ -16,7 +17,7 @@ from src.core.rogo.exceptions import InvalidSettingsError
 if TYPE_CHECKING:
     from src.core.rogo.type_aliases import Settings
 
-settings: Settings = {
+settings_no_question_type: Settings = {
     "exclude-verb-present-active-indicative": False,
     "exclude-verb-imperfect-active-indicative": False,
     "exclude-verb-future-active-indicative": False,
@@ -129,7 +130,124 @@ def test_no_question_type():
     vocab_list = read_vocab_file(Path("tests/lego_test/testdata/regular_list.txt"))
 
     with pytest.raises(InvalidSettingsError) as error:
-        for _ in ask_question_without_sr(vocab_list, 1, settings):
+        for _ in ask_question_without_sr(vocab_list, 1, settings_no_question_type):
             ...
 
     assert str(error.value) == "No question type has been enabled."
+
+
+settings_no_words: Settings = {
+    "exclude-verb-present-active-indicative": False,
+    "exclude-verb-imperfect-active-indicative": False,
+    "exclude-verb-future-active-indicative": False,
+    "exclude-verb-perfect-active-indicative": False,
+    "exclude-verb-pluperfect-active-indicative": False,
+    "exclude-verb-future-perfect-active-indicative": False,
+    "exclude-verb-present-active-infinitive": False,
+    "exclude-verb-present-passive-infinitive": False,
+    "exclude-verb-present-active-imperative": False,
+    "exclude-verb-present-active-subjunctive": False,
+    "exclude-verb-imperfect-active-subjunctive": False,
+    "exclude-verb-perfect-active-subjunctive": False,
+    "exclude-verb-pluperfect-active-subjunctive": False,
+    "exclude-verb-present-passive-indicative": False,
+    "exclude-verb-imperfect-passive-indicative": False,
+    "exclude-verb-future-passive-indicative": False,
+    "exclude-verb-perfect-passive-indicative": False,
+    "exclude-verb-pluperfect-passive-indicative": False,
+    "exclude-verb-future-perfect-passive-indicative": False,
+    "exclude-verb-singular": False,
+    "exclude-verb-plural": False,
+    "exclude-verb-1st-person": False,
+    "exclude-verb-2nd-person": False,
+    "exclude-verb-3rd-person": False,
+    "exclude-participles": False,
+    "exclude-participle-present-active": False,
+    "exclude-participle-perfect-passive": False,
+    "exclude-participle-future-active": False,
+    "exclude-participle-masculine": False,
+    "exclude-participle-feminine": False,
+    "exclude-participle-neuter": False,
+    "exclude-participle-nominative": False,
+    "exclude-participle-vocative": False,
+    "exclude-participle-accusative": False,
+    "exclude-participle-genitive": False,
+    "exclude-participle-dative": False,
+    "exclude-participle-ablative": False,
+    "exclude-participle-singular": False,
+    "exclude-participle-plural": False,
+    "exclude-noun-nominative": False,
+    "exclude-noun-vocative": False,
+    "exclude-noun-accusative": False,
+    "exclude-noun-genitive": False,
+    "exclude-noun-dative": False,
+    "exclude-noun-ablative": False,
+    "exclude-noun-singular": False,
+    "exclude-noun-plural": False,
+    "exclude-adjective-masculine": False,
+    "exclude-adjective-feminine": False,
+    "exclude-adjective-neuter": False,
+    "exclude-adjective-nominative": False,
+    "exclude-adjective-vocative": False,
+    "exclude-adjective-accusative": False,
+    "exclude-adjective-genitive": False,
+    "exclude-adjective-dative": False,
+    "exclude-adjective-ablative": False,
+    "exclude-adjective-singular": False,
+    "exclude-adjective-plural": False,
+    "exclude-adjective-positive": False,
+    "exclude-adjective-comparative": False,
+    "exclude-adjective-superlative": False,
+    "exclude-adverbs": False,
+    "exclude-adverb-positive": False,
+    "exclude-adverb-comparative": False,
+    "exclude-adverb-superlative": False,
+    "exclude-pronoun-masculine": False,
+    "exclude-pronoun-feminine": False,
+    "exclude-pronoun-neuter": False,
+    "exclude-pronoun-nominative": False,
+    "exclude-pronoun-vocative": False,
+    "exclude-pronoun-accusative": False,
+    "exclude-pronoun-genitive": False,
+    "exclude-pronoun-dative": False,
+    "exclude-pronoun-ablative": False,
+    "exclude-pronoun-singular": False,
+    "exclude-pronoun-plural": False,
+    "exclude-nouns": False,
+    "exclude-verbs": False,
+    "exclude-deponents": False,
+    "exclude-adjectives": False,
+    "exclude-pronouns": False,
+    "exclude-regulars": False,
+    "exclude-verb-first-conjugation": False,
+    "exclude-verb-second-conjugation": False,
+    "exclude-verb-third-conjugation": False,
+    "exclude-verb-fourth-conjugation": False,
+    "exclude-verb-mixed-conjugation": False,
+    "exclude-verb-irregular-conjugation": False,
+    "exclude-noun-first-declension": False,
+    "exclude-noun-second-declension": False,
+    "exclude-noun-third-declension": False,
+    "exclude-noun-fourth-declension": False,
+    "exclude-noun-fifth-declension": False,
+    "exclude-noun-irregular-declension": False,
+    "exclude-adjective-212-declension": False,
+    "exclude-adjective-third-declension": False,
+    "english-subjunctives": True,
+    "include-typein-engtolat": True,
+    "include-typein-lattoeng": True,
+    "include-parse": True,
+    "include-inflect": True,
+    "include-principal-parts": True,
+    "include-multiplechoice-engtolat": True,
+    "include-multiplechoice-lattoeng": True,
+    "number-multiplechoice-options": 3,
+}
+
+
+def test_ask_questions_empty_vocab_raises():
+    with pytest.raises(InvalidSettingsError) as exc:
+        for _ in ask_question_without_sr(VocabList(vocab=[], vocab_list_text=""), 1, settings_no_words):
+            ...
+
+    assert "No words in the vocabulary list after filtering." in str(exc.value)
