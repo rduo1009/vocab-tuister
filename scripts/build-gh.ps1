7ad52c7b6e5fee2adf200c290e5c2683d0d715e1
rduo1009 <robertduong09alt@gmail.com>
2025-07-05 03:41:13 +0000

    🐛 fix: trying to fix 8

--- scripts/build-gh.ps1
+++ scripts/build-gh.ps1
@@ -10,17 +10,17 @@ if ($env:debug -eq "True") {
 }
 
 # Only install necessary deps to speed up build
-& "%APPDATA%\pypoetry\venv\Scripts\poetry" sync --only main # slower but works better?
-# & "%APPDATA%\pypoetry\venv\Scripts\poetry" env remove --all
-# & "%APPDATA%\pypoetry\venv\Scripts\poetry" install --only main
+& "$env:APPDATA\pypoetry\venv\Scripts\poetry" sync --only main # slower but works better?
+# & "$env:APPDATA\pypoetry\venv\Scripts\poetry" env remove --all
+# & "$env:APPDATA\pypoetry\venv\Scripts\poetry" install --only main
 
 # Build python server
-& "%APPDATA%\pypoetry\venv\Scripts\poetry" run dunamai from any | Set-Content -Path "__version__.txt"
+& "$env:APPDATA\pypoetry\venv\Scripts\poetry" run dunamai from any | Set-Content -Path "__version__.txt"
 if ($env:debug -eq "True") {
-    & "%APPDATA%\pypoetry\venv\Scripts\poetry" run pyinstaller vocab-tuister-server.spec -- --clean
+    & "$env:APPDATA\pypoetry\venv\Scripts\poetry" run pyinstaller vocab-tuister-server.spec -- --clean
 }
 else {
-    & "%APPDATA%\pypoetry\venv\Scripts\poetry" run pyinstaller vocab-tuister-server.spec
+    & "$env:APPDATA\pypoetry\venv\Scripts\poetry" run pyinstaller vocab-tuister-server.spec
 }
 
 # Determine client binary name
@@ -34,7 +34,7 @@ else {
 }
 
 # Build go client
-$version = (& "%APPDATA%\pypoetry\venv\Scripts\poetry" run dunamai from any)
+$version = (& "$env:APPDATA\pypoetry\venv\Scripts\poetry" run dunamai from any)
 go mod tidy
 # HACK: go generate is not working properly; but problems will be caught by other runs
 # go generate -x ./...; if (-not (& "C:\Program Files\Git\bin\git.exe" diff --quiet)) { Write-Error "Error: Code changes after go generate."; exit 1 }
@@ -48,8 +48,8 @@ go build `
 # Write-Host ""
 # if ([string]::IsNullOrEmpty($response)) { $response = "Y" }
 # if ($response -eq "y" -or $response -eq "Y") {
-#     & "%APPDATA%\pypoetry\venv\Scripts\poetry" install --sync
+#     & "$env:APPDATA\pypoetry\venv\Scripts\poetry" install --sync
 # }
 # else {
-#     & "%APPDATA%\pypoetry\venv\Scripts\poetry" install --only main --sync
+#     & "$env:APPDATA\pypoetry\venv\Scripts\poetry" install --only main --sync
 # }
