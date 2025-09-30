2d1c35e7826885ffbd8f0b7863716d1d5671b9c4
rduo1009 <robertduong09alt@gmail.com>
2025-09-30 15:56:45 +1000

    🚀 perf: general performance improvements (#335)
    * 🚀 perf: reworking multiple choice to remove copying
    
    * 🚀 perf: adding cache to `lemmainflect.get_inflection` and `find_synonyms`
    
    * 🚀 perf: do not decompress wordnet if synonyms are not being used
    
    * 🐛 fix: fix incorrect implementation of `_get_inflection_first`
    
    (cherry picked from commit 845127a7b6c1ba88dc35b3428e0db1eac8ebf4d5)
    
    * 🧹 chore: remove todo resolved by 6102030
    
    * 🚨 tests: add `setup_wn` to test startup
    
    * 🚨 tests: update integration test snapshots

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,16 @@
 import icecream
 import pytest
+from src.core.transfero.synonyms import setup_wn
 
 
 @pytest.fixture(scope="session", autouse=True)
 def install_icecream():
     icecream.install()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def initialise_wn():
+    setup_wn()
 
 
 def pytest_configure(config):
