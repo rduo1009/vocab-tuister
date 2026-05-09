8e9f03a4abd2f16bae0cf0944902b8ea485aac7a
rduo1009 <robertduong09alt@gmail.com>
2026-05-09 19:04:04 +1000

    🚨 tests: cleanup tests

--- tests/integration/test_server_session_integration.py
+++ tests/integration/test_server_session_integration.py
@@ -1,7 +1,6 @@
 # pyright: basic
 # pyright: reportUnknownParameterType=false, reportUnknownArgumentType=false, reportMissingParameterType=false
 
-import asyncio
 import json
 from pathlib import Path
 
@@ -9,20 +8,18 @@ import pytest
 from grpclib import GRPCError
 from src.core.rogo.type_aliases import Settings
 from src.pb.vocab_tuister.v1 import CreateSessionRequest, SessionConfig, VerifyConfigRequest, VerifyVocabRequest
-from src.server.app import VocabTesterService, run
+from src.server.app import VocabTesterService
 
 
 @pytest.fixture
 def setup_tests(monkeypatch):
-    async def _setup(port, vocab_file_info, session_config_info):
+    def _setup(vocab_file_info, session_config_info):
         class _Dirs:
             user_config_path = Path(__file__).parent / "testdata"
             user_cache_path = Path(__file__).parent / "testdata" / "cache"
 
         monkeypatch.setattr("src.server.app.dirs", _Dirs)
         monkeypatch.setattr("src.server.app.settings", Settings.model_validate_json((Path(_Dirs.user_config_path) / "settings.json").read_text()))
-
-        server_url = f"127.0.0.1:{port}"
 
         vocab_file = Path(__file__).parent / "testdata" / f"test-{vocab_file_info}-list.txt"
         vocab_text = vocab_file.read_text(encoding="utf-8")
@@ -31,10 +28,7 @@ def setup_tests(monkeypatch):
         session_config_dict = json.loads(session_config_file.read_text(encoding="utf-8"))
         session_config = SessionConfig.from_dict(session_config_dict)
 
-        task = asyncio.create_task(run(port))
-        await asyncio.sleep(0.2)
-
-        return server_url, vocab_text, session_config, task
+        return vocab_text, session_config
 
     return _setup
 
@@ -42,7 +36,7 @@ def setup_tests(monkeypatch):
 @pytest.mark.integration
 @pytest.mark.asyncio
 async def test_cli_normal(snapshot, setup_tests):
-    _, vocab_text, session_config, server = await setup_tests(5600, "regular", "regular")
+    vocab_text, session_config = setup_tests("regular", "regular")
 
     client = VocabTesterService()
 
@@ -57,13 +51,11 @@ async def test_cli_normal(snapshot, setup_tests):
     session_response = [msg.to_dict() async for msg in stream]
     assert session_response == snapshot
 
-    server.cancel()
-
 
 @pytest.mark.integration
 @pytest.mark.asyncio
 async def test_cli_error_list(snapshot, setup_tests):
-    _, vocab_text, _, server = await setup_tests(5601, "error", "regular")
+    vocab_text, _ = setup_tests("error", "regular")
 
     client = VocabTesterService()
 
@@ -72,13 +64,11 @@ async def test_cli_error_list(snapshot, setup_tests):
 
     assert exc_info.value == snapshot
 
-    server.cancel()
-
 
 @pytest.mark.integration
 @pytest.mark.asyncio
 async def test_cli_error_numberquestions1_config(snapshot, setup_tests):
-    _, _, session_config, server = await setup_tests(5600, "regular", "regular")
+    _, session_config = setup_tests("regular", "regular")
 
     client = VocabTesterService()
 
@@ -87,13 +77,11 @@ async def test_cli_error_numberquestions1_config(snapshot, setup_tests):
 
     assert exc_info.value == snapshot
 
-    server.cancel()
-
 
 @pytest.mark.integration
 @pytest.mark.asyncio
 async def test_cli_error_numberquestions2_config(snapshot, setup_tests):
-    _, vocab_text, session_config, server = await setup_tests(5600, "regular", "regular")
+    vocab_text, session_config = setup_tests("regular", "regular")
 
     client = VocabTesterService()
     stream = client.create_session(CreateSessionRequest(vocab_list=vocab_text, number_of_questions=-1, session_config=session_config))
@@ -102,5 +90,3 @@ async def test_cli_error_numberquestions2_config(snapshot, setup_tests):
         [msg.to_dict() async for msg in stream]
 
     assert exc_info.value == snapshot
-
-    server.cancel()
